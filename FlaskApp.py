172fda1c5697a9d4d8ba5c5049476b808014d1be
nhayes-roth <nathan.hayesroth@gmail.com>
2014-02-23 18:30:21 -0500

    Page 29 of Flask-docs

--- FlaskApp.py
+++ FlaskApp.py
@@ -1,9 +1,33 @@
-# import the Flask class
-from flask import Flask
+# import statements
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
-app = Flask(__name__)	# instance of Flask class
-						# __name__ is name of the application's module
+# create the application?
+app = Flask(__name__)
+app.config.from_object(__name__)
 
+# load default config and override config from an environment variable
+app.config.update(dict(
+	DATABASE	= os.path.join(app.root_path, 'FlaskApp.db'),
+	DEBUG		= True,
+	SECRET_KEY	= 'development key',
+	USERNAME	= 'user',
+	PASSWORD	= 'password'
+))
+app.config.from_envvar('FLASKAPP_SETTINGS', silent = True)
+
+# connect to the database
+def connect_db():
+	rv = sqlite3.connect(app.config['DATABASE'])
+	rv.row_factory = sqlite3.Row
+	return rv
+
+# start the server to run this file as a standalone application
+if __name__ == '__main__':		# only run script if executed in Python interpreter, and not loaded as a module
+	app.run()
+	# app.run('0.0.0.0')		# run the server publicly
+
+""" OLD ROUTES
 # index page
 @app.route('/')
 def index():
@@ -29,8 +53,4 @@ def micropost(user_name, post_id):
 @app.route('/hello/')			# what url should trigger our function
 def hellow_word():
 	return "Hello world!"
-
-if __name__ == '__main__':		# only run script if executed in Python interpreter, and not loaded as a module
-	app.debug = True			# enable debug mode
-	app.run()
-	# app.run('0.0.0.0')		# run the server publicly
+"""

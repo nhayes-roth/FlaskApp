f0cde49fc3c82315548931d5094889a667c65baf
nhayes-roth <nathan.hayesroth@gmail.com>
2014-02-23 13:43:14 -0500

    Commented hello, world

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
+# import the Flask class
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
+app = Flask(__name__)	# instance of Flask class
+						# __name__ is name of the application's module
+
+@app.route('/hello')			# what url should trigger our function
 def hellow_word():
 	return "Hello world!"
 
-if __name__ == '__main__':
+if __name__ == '__main__':		# only run script if executed in Python interpreter, and not loaded as a module
 	app.run()
+	# app.run('0.0.0.0')		# run the server publicly
